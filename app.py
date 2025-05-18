f3252b2bba25c1b8558e0864cb63d91e12a1e5a5
McHill007 <32106919+McHill007@users.noreply.github.com>
2025-05-18 11:55:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,22 +6,32 @@ app = Flask(__name__)
 
 @app.route("/anonymize", methods=["POST"])
 def anonymize():
-    data = request.get_json()
-    text = data.get("text", "")
-
-    if not text:
-        return jsonify({"error": "Text is required"}), 400
-
     try:
+        import os
+        print("Working dir:", os.getcwd())
+        print("YAML exists:", os.path.exists("analyzer_config.yml"))
+
+        data = request.get_json(force=True)
+        text = data.get("text", "")
+
+        if not text:
+            return jsonify({"error": "Text is required"}), 400
+
+        from pii_anonymizer import PiiAnonymizer
         original, anonymized, lang, entities = PiiAnonymizer.anonymize(text)
+
         return jsonify({
             "language": lang,
             "original": original,
             "anonymized": anonymized,
             "entities": entities
         })
+
     except Exception as e:
+        import traceback
+        traceback.print_exc()
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
